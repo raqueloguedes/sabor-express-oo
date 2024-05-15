e4fc272e8cb8f4d18090e960c89394bd181bd78d
Ana Raquel Guedes <araqueloguedes@gmail.com>
2024-05-15 05:58:55 -0300

    construtor e instanciando objetos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,14 +1,25 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    #nome da clss sempre com letra maiuscula
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __init__(self, nome, categoria):
+        # o init é o "construtor" de uma classe em Python, responsável por inicializar os objetos dessa classe com valores específicos.
+        # self é tipo individual daquele que ta sendo referido
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+        #append vai inserir na lista todo objt criado
+    
+    def __str__(self):
+        #metodo especial str vai transformar aquela numeração do objeto em strig
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print(restaurante_praca.ativo)
+Restaurante.listar_restaurantes()
