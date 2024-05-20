880b07e95e1587e5a9583067e9ffdafcf9c5060a
Ana Raquel Guedes <araqueloguedes@gmail.com>
2024-05-20 18:53:09 -0300

    projeto antes da api

--- modelos/cardapio/item_cardapio.py
+++ modelos/cardapio/item_cardapio.py
@@ -1,4 +1,11 @@
-class ItemCardapio:
+from abc import ABC, abstractmethod
+
+class ItemCardapio(ABC):
     def __init__(self,nome,preco):
         self._nome = nome
         self._preco = preco
+
+    @abstractmethod
+    #esse método sirva como um modelo para que outras classes derivadas o implementem.
+    def aplicar_desconto(self):
+        pass
--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -8,4 +8,9 @@ class Prato(ItemCardapio):
 
     def __str__(self):
         return self._nome   
+    
+    def aplicar_desconto(self):
+        #metodo abstrato 
+        #obrigatorio colocar aqui no elemento filho mas poderia ser o pass que seria tipo em branco
+        self._preco -= (self._preco * 0.05)
  
